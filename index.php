149a1d8c2d656a682947fd6c3fada42359bd2eaa
Dyar Wafa <165853643+dyar-21@users.noreply.github.com>
2024-09-28 13:12:54 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Profile</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="img">
+   <img src="halabja.png" width="100px">
+   </div>
+   <header>Student Profile</header>
+
+    <div class="container">
+
+        <form class="input_box" action="" method="post">
+    <input type="number" name="user" placeholder="Username"><br><br>
+    <input type="password" name="pass" placeholder="Password"><br>
+       <button class="btn" type="submit">Log In</button>
+        </form>
+    </div>
+
+</body>
+</html>
